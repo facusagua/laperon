53e3db024ca13e4fc060db589dab9c197be33148
facusagua <facusagua@hotmail.com>
2016-02-20 00:44:49 -0300

    Branch-1902: Modificacion imagenes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,10 +3,8 @@ jQuery(document).ready(function() {
 	
     /*
         Fullscreen background
-    */    
-    $.backstretch([
-                           "assets/img/backgrounds/121.jpg"
-                         ], {duration: 3000, fade: 750});
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
     
     $('#top-navbar-1').on('shown.bs.collapse', function(){
     	$.backstretch("resize");
